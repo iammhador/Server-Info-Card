a682bdb166eb3826618091076a6bf5c9df0baa2c
FarhanZizz <iamfarhanaziz@gmail.com>
2023-02-05 14:15:38 +0530

    registration api complete

--- index.js
+++ index.js
@@ -9,41 +9,32 @@ app.use(cors());
 app.use(express.json());
 
 //# MongoDB:
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://InfoCard:${process.env.DB_PASS}>@cluster0.hilqutw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hilqutw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
+console.log(uri);
 async function run() {
   try {
     const userCollection = client.db("infocard").collection("user");
     //Registrattion API
     app.post("/registration", async (req, res) => {
-      try {
-        const { email, username } = req.body;
-        const userExist = await userCollection.findOne({
-          $or: [{ email }, { username }],
-        });
-        if (userExist) {
-          return res.status(400).send({
-            message: userExist.email
-              ? "Email address is already registered"
-              : "username is taken",
-          });
-        }
-
-        const newUser = { email, username };
-        const result = await userCollection.insertOne(newUser);
-        return res.send(result);
-      } catch (error) {
-        return res
-          .status(500)
-          .send({ message: "An error occurred while registering try again" });
+      const { email, username } = req.body;
+      const usernameExist = await userCollection.findOne({ username });
+      const emailExist = await userCollection.findOne({ email });
+      if (emailExist) {
+        return res.status(400).send({ message: "Email is already registered" });
       }
+      if (usernameExist) {
+        return res.status(400).send({ message: "username is taken" });
+      }
+
+      const newUser = { email, username };
+      const result = await userCollection.insertOne(newUser);
+      return res.send(result);
     });
   } finally {
   }
