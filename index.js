89d678d6d1d6a1cd06060107e7f55e02336b4c52
mehedy3107 <mhador111@gmail.com>
2023-02-07 19:29:36 +0600

    User Router Created

--- index.js
+++ index.js
@@ -10,41 +10,60 @@ app.use(express.json());
 
 //# MongoDB:
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { query } = require("express");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hilqutw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
 async function run() {
   try {
     const userCollection = client.db("infocard").collection("user");
     //Registrattion API
-    app.post("/registration", async (req, res) => {
-      try {
-        const { email, username } = req.body;
-        const userExist = await userCollection.findOne({
-          $or: [{ email }, { username }],
-        });
-        if (userExist) {
-          return res.status(400).send({
-            message: userExist.email
-              ? "Email address is already registered"
-              : "username is taken",
-          });
-        }
 
-        const newUser = { email, username };
-        const filter = req.body;
-        console.log(req.body);
-        const result = await userCollection.insertOne(filter);
-        return res.send(result);
-      } catch (error) {
-        return res
-          .status(500)
-          .send({ message: "An error occurred while registering try again" });
+    app.get("/users", async (req, res) => {
+      let filter = {};
+      // console.log(req.query);
+
+      if (req.query.username) {
+        filter = {
+          username: req.query.username,
+        };
       }
+      const result = await userCollection.find(filter).toArray();
+      return res.send(result);
+    });
+
+    // app.get("/users/:email", async (req, res) => {
+    // console.log(req.params);
+    //   const email = req.params.email;
+    //   const query = { email };
+    //   const result = await userCollection.findOne(query);
+    //   res.send(result);
+    // });
+
+    // app.get("/users/:username", async (req, res) => {
+    //   const username = req.params.username;
+    //   const query = { username };
+    //   const result = await userCollection.findOne(query);
+    //   return res.send(result);
+    // });
+
+    app.post("/registration", async (req, res) => {
+      const { email, username } = req.body;
+      const usernameExist = await userCollection.findOne({ username });
+      const emailExist = await userCollection.findOne({ email });
+      if (emailExist) {
+        return res.status(400).send({ message: "Email is already registered" });
+      }
+      if (usernameExist) {
+        return res.status(400).send({ message: "username is taken" });
+      }
+
+      const newUser = { email, username };
+      const result = await userCollection.insertOne(newUser);
+      return res.send(result);
     });
   } finally {
   }
